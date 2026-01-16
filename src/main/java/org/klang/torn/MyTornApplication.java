c5f0e36c203b70abc897c67db036452d9cd1e227
chaymus.klang <chaymus.klang@gmail.com>
2026-01-16 14:07:27 -0600

    Init project

---
+++ src/main/java/org/klang/torn/MyTornApplication.java
@@ -0,0 +1,13 @@
+package org.klang.torn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/klang/torn/MyTornApplicationTests.java
@@ -0,0 +1,13 @@
+package org.klang.torn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
